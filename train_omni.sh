095af4a86c27c41eaa7388064b90466ecef95a0d
apeterswu <apeterswu@gmail.com>
2018-07-25 12:53:28 +0800

    script for training on philly wu2

--- train_omni.sh
+++ train_omni.sh
@@ -1,3 +1,6 @@
+nvidia-smi
+
+pwd
 REMOTE_CODE_PATH=/hdfs/sdrgvc/v-lijwu/bilm-tf-master
 DATA_PATH=$REMOTE_CODE_PATH/data
 
@@ -6,7 +9,7 @@ export PYTHONPATH=$REMOTE_CODE_PATH:$PYTHONPATH
 
 # export CUDA_VISIBLE_DEVICES=0,1,2
 # export CUDA_VISIBLE_DEVICES=0,1,2,3
-python $REMOTE_CODE_PATH/bin/train_omni_elmo.py 
+python3 $REMOTE_CODE_PATH/bin/train_omni_elmo.py \
     --train_prefix=$DATA_PATH/1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled/* \
     --vocab_file=$DATA_PATH/vocab-2016-09-10.txt \
-    --save_dir=$REMOTE_CODE_PATH/mode_omni
+    --save_dir=$DATA_PATH/model_omni
