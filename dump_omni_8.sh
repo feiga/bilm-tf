4e62c331240b977388ff5c2f7783a7768aa04ec2
apeterswu <apeterswu@gmail.com>
2018-08-15 14:47:13 +0800

    Add dump weights to hdf5 files

---
+++ dump_omni_8.sh
@@ -0,0 +1,13 @@
+nvidia-smi
+
+pwd
+REMOTE_CODE_PATH=/hdfs/sdrgvc/v-lijwu/bilm-tf-master
+
+export PYTHONPATH=$REMOTE_CODE_PATH:$PYTHONPATH
+# sudo pip install h5py
+
+# export CUDA_VISIBLE_DEVICES=0,1,2
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+python3 $REMOTE_CODE_PATH/bin/dump_weights.py \
+    --save_dir=$REMOTE_CODE_PATH/model_omni_8 \
+    --outfile=$REMOTE_CODE_PATH/model_omni_8/omni_8_weight.hdf5
